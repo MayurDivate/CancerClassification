5de1af90e25a30cae18b5a883a1b420f91b5d8ed
Mayur Divate <mayur.divate91@gmail.com>
2019-08-01 15:43:18 +1000

    row and column filters added

--- Matrix/filters.py
+++ Matrix/filters.py
@@ -39,3 +39,34 @@ class SampleFilter:
         matrix.to_csv(self.outfile, sep='\t')
         print(matrix.info())
         return matrix
+
+class MatrixFilter:
+
+    def __init__(self, matrix_file, name_list_file, outfile):
+        self.matrix_file = matrix_file
+        self.name_list_file = name_list_file
+        self.outfile = outfile
+
+    def get_name_list(self):
+        return [line.rstrip() for line in open(self.name_list_file, 'r')]
+
+class RowFilter(MatrixFilter):
+
+    def apply_filter(self):
+        matrix = pd.read_csv(self.matrix_file, sep='\t', index_col=0)
+        row_ids = self.get_name_list()
+
+        matrix = matrix.reindex(row_ids).dropna(how='all')
+        print(matrix.info())
+        matrix.to_csv(self.outfile, sep='\t')
+        return matrix
+
+class ColumnFilter(MatrixFilter):
+
+    def apply_filter(self):
+        matrix = pd.read_csv(self.matrix_file, sep='\t', index_col=0)
+        column_ids = self.get_name_list()
+        matrix = matrix[column_ids]
+        print(matrix.info())
+        matrix.to_csv(self.outfile, sep='\t')
+        return matrix
