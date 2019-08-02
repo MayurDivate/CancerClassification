51971aa7a75a74f4ffc7dda600798d6e0fa1c68b
Mayur Divate <mayur.divate91@gmail.com>
2019-08-02 10:58:50 +1000

    Runnner is  running

--- Runner/3_Run_model.py
+++ Runner/3_Run_model.py
@@ -1,13 +1,29 @@
-from DeepLearning.data_preprocess import Preprocessor
+import os
+
+from Datasets.utils import create_output_dir
+from DeepLearning.train_model import ModelTrainer
+
+data_dir = '/Users/n10337547/Projects/1_CUP/1_Blood_Cancer/2_DL_data/1_BBS_classification/3_Train_Val_Test_mats'
+
+train = os.path.join(data_dir, 'train_mat.tsv')
+test = os.path.join(data_dir, 'test_mat.tsv')
+val = os.path.join(data_dir, 'val_mat.tsv')
 
 
 def train_cnn_model():
 
-    # get the trraining data
-    ptrain =  Preprocessor()
+    resdir = '/Users/n10337547/Projects/1_CUP/1_Blood_Cancer/3_Results/1_BBS_classification'
 
-    # get the model
+    create_output_dir(resdir)
 
-    # train the model
+    cnn_trainer = ModelTrainer(train_tsv=train, test_tsv=test, val_tsv=val, nfeatures=13565, nclasses=5,
+                               mname="Blood_Breast_Skin_CG_CNN", outdir=resdir)
 
-    # save the model
+    model, res = cnn_trainer.run_cnn_model(test=True)
+
+    model_file = os.path.join(resdir, "Blood_Breast_Skin_CG_CNN.h5")
+    model.save(model_file)
+    print('done')
+
+
+train_cnn_model()
