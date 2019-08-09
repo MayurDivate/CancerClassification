66c5ab50cc629bf4d24001a3c301bb3e1b6c5e44
Mayur Divate <mayur.divate91@gmail.com>
2019-08-09 16:02:28 +1000

    random seed added

--- DeepLearning/models.py
+++ DeepLearning/models.py
@@ -1,4 +1,5 @@
 import tensorflow as tf
+from numpy.random import seed
 
 
 # Model
@@ -15,19 +16,20 @@ class DLmodel:
 
     # method for 1D CNN model
     def get_1D_cnn_model(self):
+        seed(7)
+        tf.random.set_seed(3)
+
         model = tf.keras.Sequential()
-        model.add(tf.keras.layers.Conv1D(8, kernel_size=1, activation='relu', input_shape=(1, self.nfeatures)))
+        model.add(tf.keras.layers.Conv1D(10, kernel_size=1, activation='relu', input_shape=(1, self.nfeatures)))
         model.add(tf.keras.layers.Flatten())
-        model.add(tf.keras.layers.Dense(130, activation='relu'))
-        model.add(tf.keras.layers.Dense(130, activation='relu'))
-        model.add(tf.keras.layers.Dense(130, activation='relu'))
-        model.add(tf.keras.layers.Dense(130, activation='relu'))
-        model.add(tf.keras.layers.Dense(130, activation='relu'))
+        model.add(tf.keras.layers.Dense(20, activation='relu'))
+        model.add(tf.keras.layers.Dense(20, activation='relu'))
+        model.add(tf.keras.layers.Dense(20, activation='relu'))
 
         model.add(tf.keras.layers.Dense(self.nclasses, activation='softmax'))
 
         ## optimizer
-        opt = tf.keras.optimizers.RMSprop(lr=0.00001)
+        opt = tf.keras.optimizers.RMSprop(lr=0.0001)
 
         ## Model compile
         model.compile(optimizer=opt, loss='categorical_crossentropy', metrics=['accuracy'])
@@ -37,6 +39,9 @@ class DLmodel:
 
     # method for MLP model
     def get_mlp_model(self):
+        seed(7)
+        tf.random.set_seed(3)
+
         model = tf.keras.Sequential()
 
         model.add(tf.keras.layers.Dense(300, activation='relu', input_shape=(self.nfeatures,)))
