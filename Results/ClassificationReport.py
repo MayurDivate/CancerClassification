759a7450aab03fcb7da5eca2d91ff157d0300072
Mayur Divate <mayur.divate91@gmail.com>
2019-09-18 11:50:09 +1000

    labels, lfile and example

--- Results/ClassificationReport.py
+++ Results/ClassificationReport.py
@@ -1,20 +1,21 @@
-import numpy as np
-import pandas as pd
 import os
 
-from sklearn.metrics import confusion_matrix
+import numpy as np
+import pandas as pd
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
 
 
-class ConfusionMatrix:
+class ClassificationReport:
 
-    def __init__(self, model_res_file, ypred_col, real_col, outdir):
+    def __init__(self, model_res_file, ypred_col, lfile, real_col, outdir):
         self.model_res_file = model_res_file
         self.ypred_col = ypred_col
         self.real_col = real_col
-        lfile = '/Users/n10337547/Projects/1_CUP/2_TCGA/0_Data/TCGA_classes.txt'
-        self.labels = [label.rstrip() for label in open(lfile,'r')]
+
+        self.lfile = lfile
+        self.labels = [label.rstrip() for label in open(self.lfile, 'r')]
         self.outdir = outdir
 
     def parse_results(self):
@@ -22,15 +23,13 @@ class ConfusionMatrix:
         ypred = np.array(df.iloc[:, self.ypred_col])
         yreal = np.array(df.iloc[:, self.real_col])
 
-
-        res =  confusion_matrix(yreal, ypred)
+        res = confusion_matrix(yreal, ypred)
         print(res)
-        acc  = accuracy_score(yreal, ypred)
+        acc = accuracy_score(yreal, ypred)
         print(acc)
         report = classification_report(yreal, ypred)
         print(report)
         out = os.path.join(self.outdir, 'classification_report.txt')
-
 
         with open(out, 'w') as f:
             f.write(report)
@@ -38,8 +37,10 @@ class ConfusionMatrix:
         print('Done')
 
 
-model_1 = '/Users/n10337547/Projects/model_2_pred.txt'
-out = '/Users/n10337547/Projects/3_Symposium/3_Results'
+# Example
 
-model_1_conf = ConfusionMatrix(model_1, 78, 79, out)
-model_1_conf.parse_results()
+# model_1 = '/Users/n10337547/Projects/model_2_pred.txt'
+# out = '/Users/n10337547/Projects/3_Symposium/3_Results'
+# lfile = '/Users/n10337547/Projects/1_CUP/2_TCGA/0_Data/TCGA_classes.txt'
+# model_1_conf = ClassificationReport(model_1, 78, 79, lfile, out)
+# model_1_conf.parse_results()
